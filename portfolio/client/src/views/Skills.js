2090ad0dcda43e901d01fb34db2f7ce45e5a1ed3
Antholem <antholemlemmanalo@gmail.com>
2025-01-09 18:28:56 +0800

    Skill sets

--- portfolio/client/src/views/Skills.js
+++ portfolio/client/src/views/Skills.js
@@ -1,12 +1,58 @@
 import React from "react";
-import { FaLaptopCode } from "react-icons/fa";
+import * as SiIcons from "react-icons/si"
 import { Card } from "../components";
 
 // Skills data
-const skills = Array(5).fill({
-    icon: <FaLaptopCode className="text-brand text-5xl mb-4" />,
-    title: "Skills",
-});
+const skills = [
+    { 
+        title: "HTML", 
+        icon: <SiIcons.SiHtml5 className="text-brand text-7xl" />,
+    },
+    {
+        title: "CSS",
+        icon: <SiIcons.SiCss3 className="text-brand text-7xl" />,
+    },
+    {
+        title: "EJS",
+        icon: <SiIcons.SiJavascript className="text-brand text-7xl" />,
+    },
+    {
+        title: "Tailwind",
+        icon: <SiIcons.SiTailwindcss className="text-brand text-7xl" />,
+    },
+    {
+        title: "Chakra UI",
+        icon: <SiIcons.SiChakraui className="text-brand text-7xl" />,
+    },
+    {
+        title: "Material UI",
+        icon: <SiIcons.SiMui className="text-brand text-7xl" />,
+    },
+    {
+        title: "NextJS",
+        icon: <SiIcons.SiNextdotjs className="text-brand text-7xl" />,
+    },
+    {
+        title: "ReactJS",
+        icon: <SiIcons.SiReact className="text-brand text-7xl" />,
+    },
+    {
+        title: "NodeJS",
+        icon: <SiIcons.SiNodedotjs className="text-brand text-7xl" />,
+    },
+    {
+        title: "ExpressJS",
+        icon: <SiIcons.SiExpress className="text-brand text-7xl" />,
+    },
+    {
+        title: "MongoDB",
+        icon: <SiIcons.SiMongodb className="text-brand text-7xl" />,
+    },
+    {
+        title: "MySQL",
+        icon: <SiIcons.SiMysql className="text-brand text-7xl" />,
+    },
+];
 
 const Skills = () => {
     return (
@@ -20,19 +66,23 @@ const Skills = () => {
             </header>
 
             {/* Skills Grid */}
-            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 mx-auto">
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-8 mx-auto">
                 {skills.map((skill, index) => (
                     <Card
                         key={index}
                         variant="elevated"
                         size="sm"
-                        className="flex flex-col items-center justify-center gap-2"
+                        className="flex flex-col items-center justify-center gap-5"
                     >
                         {/* Skill Icon */}
-                        {skill.icon}
+                        <span>
+                            {skill.icon}
+                        </span>
 
                         {/* Skill Title */}
-                        <h3 className="text-base font-semibold">{skill.title}</h3>
+                        <h3 className="text-xs">
+                            {skill.title}
+                        </h3>
                     </Card>
                 ))}
             </div>
