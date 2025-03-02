b6a9fe2750b05776916d5e32081cb7b6275a0a5d
Antholem <antholemlemmanalo@gmail.com>
2025-03-02 15:49:32 +0800

    Projects image

--- portfolio/client/src/views/Projects/index.js
+++ portfolio/client/src/views/Projects/index.js
@@ -2,25 +2,28 @@ import React from "react";
 import { useThemeStore } from "../../store/themeStore";
 import { Button, Card } from "../../components";
 import * as Icon from "react-icons/fa";
+import project_1 from "./project-1.png";
+import project_2 from "./project-2.png";
+import project_3 from "./project-3.png";
 
 // Projects data
 const projects = [
     {
-        image: "https://fakeimg.pl/1200x800/bf6b6b/8789ff?text=No+image&font=noto",
+        image: project_1,
         name: "Learn Type",
         description: "A Static Single Page Application that built entirely from Scratch Using HTML, CSS, and VanillaJS.",
         liveUrl: "https://learn-type.vercel.app/",
         sourceUrl: "https://github.com/Antholem/Learn-Type",
     },
     {
-        image: "https://placehold.co/1200x800",
+        image: project_2,
         name: "PokéAPI",
         description: "A Restful Pokémon API Application Using ReactJS and Material UI with customizable.",
         liveUrl: "https://poke-api-2023.vercel.app/",
         sourceUrl: "https://github.com/Antholem/PokeAPI",
     },
     {
-        image: "https://placehold.co/1200x800",
+        image: project_3,
         name: "CryptCoin",
         description: "Real-time cryptocurrency app using ReactJS and Material UI with Coingecko API for live data and dynamic routing.",
         liveUrl: "https://crypt-coin.vercel.app/",
@@ -50,16 +53,19 @@ const Projects = () => {
                         size="md"
                         className="flex flex-col items-start gap-6 p-6"
                     >
-                        
+
                         {/* Project Image */}
-                        {/* <img
-                            // src={project.image}
-                            alt={project.name}
-                            className="w-full h-60 object-cover rounded-lg"
-                        /> */}
-                        <div className={`w-full h-60 object-cover rounded-lg flex justify-center items-center ${theme === "dark" ? "bg-dark-action-disabled" : "bg-light-action-disabled"}`}>
-                            <Icon.FaImage className={`text-9xl ${theme === "dark" ? "text-dark-text-disabled" : "text-light-text-disabled"}`} />
-                        </div>
+                        {project.image ? (
+                            <img
+                                src={project.image}
+                                alt={project.name}
+                                className="w-full h-52 object-cover rounded-lg"
+                            />
+                        ) : (
+                            <div className={`w-full h-52 object-cover rounded-lg flex justify-center items-center ${theme === "dark" ? "bg-dark-action-disabled" : "bg-light-action-disabled"}`}>
+                                <Icon.FaImage className={`text-9xl ${theme === "dark" ? "text-dark-text-disabled" : "text-light-text-disabled"}`} />
+                            </div>
+                        )}
 
                         {/* Project Details */}
                         <div className="flex flex-col gap-2">
