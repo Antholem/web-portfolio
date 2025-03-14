6fe37074b14898aa9d4570460410ab966edf2db6
Antholem <antholemlemmanalo@gmail.com>
2025-03-14 22:41:22 +0800

    Env for firebase config

--- portfolio/client/src/config/firebase.js
+++ portfolio/client/src/config/firebase.js
@@ -8,12 +8,12 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyC3eK5dutUBa4Yiat88Ua6lGunPzZiDvLo",
-    authDomain: "web-portfolio-projects.firebaseapp.com",
-    projectId: "web-portfolio-projects",
-    storageBucket: "web-portfolio-projects.firebasestorage.app",
-    messagingSenderId: "136806660749",
-    appId: "1:136806660749:web:39f501543e1325cb30c0c4"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
