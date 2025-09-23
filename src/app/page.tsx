4e85558e0a7a418cef3987c67a48f2ebf4c1ed0c
Antholem <antholemlemmanalo@gmail.com>
2025-09-23 14:05:36 +0800

    Replace Home component's div with main structure and include ContactForm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import ContactForm from "@/components/contact-form";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <main>
+      <ContactForm />
+    </main>
+  );
 }
