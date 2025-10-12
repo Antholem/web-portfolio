b22b21bf9038fe7bda4fb6fdb58d9c8b75c8e5df
Sam Antholem Manalo <83632082+Antholem@users.noreply.github.com>
2025-10-12 09:54:35 +0800

    feat: add structured skills section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import ContactSection from "@/components/contact-section";
 import ProjectsSection from "@/components/projects-section";
+import SkillsSection from "@/components/skills-section";
 
 export default function Home() {
   return (
     <main className="space-y-2">
       <ProjectsSection />
+      <SkillsSection />
       <ContactSection />
     </main>
   );
